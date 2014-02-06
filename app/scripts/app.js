e8e2a217bb3385286c38ce6d380c4c7a07841eb7
Mark Finger <markfinger@gmail.com>
2014-02-07 10:51:48 +1100

    Mockup core.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,31 @@
-/*global define */
-define([], function () {
- 'use strict';
+define([
+  'lodash',
+  'jquery'
+], function (_, $) {
+  'use strict';
 
- return '\'Allo \'Allo!';
+  var container;
+
+  var generateImages = function() {
+    _.each(_.range(50), function(num) {
+      var image = $('<img class="comic-panel" src="assets/images/andre_limo.jpg"/>');
+      if (num === 1) {
+        image.addClass('shake');
+      }
+//      } else {
+//        image.addClass('shake-2');
+//      }
+      container.append(image);
+    });
+  };
+
+  var init = function() {
+    container = $('.comic-container');
+
+    generateImages();
+  };
+
+  return {
+    init: init
+  };
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,7 @@
 require.config({
     paths: {
         jquery: '../bower_components/jquery/jquery',
+        lodash: '../bower_components/lodash/dist/lodash',
         bootstrapAffix: '../bower_components/sass-bootstrap/js/affix',
         bootstrapAlert: '../bower_components/sass-bootstrap/js/alert',
         bootstrapButton: '../bower_components/sass-bootstrap/js/button',
@@ -56,7 +57,5 @@ require.config({
 
 require(['app', 'jquery'], function (app, $) {
     'use strict';
-    // use app here
-    console.log(app);
-    console.log('Running jQuery %s', $().jquery);
+    $(app.init);
 });
